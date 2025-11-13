631528dbf3bd1304011871332fa67336bc6f106f
Fady Mohamed <fadyyymohamed9@gmail.com>
2025-11-13 16:37:05 +0200

    create custom note item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/views/notes_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -10,8 +11,9 @@ class NotesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(brightness: Brightness.dark),
-      home: Scaffold(),
+      theme: ThemeData(brightness: Brightness.dark, fontFamily: 'Poppins'),
+
+      home: const NotesView(),
     );
   }
 }
